98aabdb7e9227aff231188dd42c51ab8420dbf14
SAM <s.hopkinson87@gmail.com>
2019-09-27 16:42:29 +0100

    Added images.
    Added javascript to the blog.

--- scripts.js
+++ scripts.js
@@ -40,3 +40,87 @@ function myFunction(x) {
   mobile.toggleClass("showme");
 }
 
+
+// BLOG PAGES SCRIPTS
+
+var blog1 = $('#blog1');
+var blog2 = $('#blog2');
+var blog3 = $('#blog3');
+var blog4 = $('#blog4');
+var blog5 = $('#blog5');
+
+var content1 = $('#content1')
+var content2 = $('#content2')
+var content3 = $('#content3')
+var content4 = $('#content4')
+var content5 = $('#content5')
+
+
+
+// add hover instructions for divs.
+
+// todo: change blog width on click
+
+blog1.click(function(){
+  if (content1.css("display") == "none") {
+    blog1.css("margin-bottom", "0");
+    blog1.animate({
+      height: "10rem",
+      width: "100%",
+        }, 500);
+  content1.slideDown(1000);
+  } 
+  else {
+    if($(window).width() < 768) {
+    blog1.animate({
+      height: "10rem",
+      width: "100%",
+    }, 500);
+    } else if(
+      $(window).width() > 768
+    ) {
+      blog1.animate({
+        height: "20rem",
+        width: "60%",
+      }, 500);
+    }
+    content1.slideUp(1000);
+    blog1.css("margin-bottom", "1rem");
+
+  }
+});
+
+blog2.click(function(){
+  if (content2.css("display") == "none") {
+    blog2.css("margin-bottom", "0");
+    blog2.animate({
+      height: "10rem",
+      width: "60%",
+        }, 500);
+        blog1.animate({
+          height: "10rem",
+          width: "30%",
+        })
+  content2.slideDown(1000);
+  } 
+  else {
+    if($(window).width() < 768) {
+    blog2.animate({
+      height: "10rem",
+      width: "100%",
+    }, 500);
+    } else if(
+      $(window).width() > 768
+    ) {
+      blog2.animate({
+        height: "20rem",
+        width: "30%",
+      }, 500);
+      blog1.animate({
+        height: "20rem",
+        width: "60%"
+      })
+    }
+    content2.slideUp(1000);
+  }
+});
