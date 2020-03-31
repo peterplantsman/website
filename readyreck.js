2f36821f1190168e012f7fc363d15439ce41b8e6
Samuel Hopkinson | Netmatters Ltd <samuel.hopkinson@netmatters.local>
2020-03-31 09:20:10 +0100

    Developing the scaffolding for the ready reckoner question pages.

--- readyreck.js
+++ readyreck.js
@@ -1,11 +1,5 @@
 // Canvas instructions
 
-// // Store
-// sessionStorage.setItem("lastname", "Smith");
-// // Retrieve
-// document.getElementById("result").innerHTML = sessionStorage.getItem("lastname");
-
-
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 context.fillStyle = "red";
@@ -17,11 +11,23 @@ var survey = {
     title: 'Calculating your garden space',
     questions: [
         {
-
-            text: "Question 1",
-        }, {
-            text: "Question 2",
-
+            text: "Please enter the dimensions of your garden space. Think of the space as roughly being a rectangle or square.",
+            answers: {
+                gardenLength: {
+                    key: 1,
+                    label: "Length (in metres)",
+                    value: null
+                },
+                gardenWidth: {
+                    key: 2,
+                    label: "Width (in metres)",
+                    value: null
+                }
+            }
+        },
+        {
+          text: "Do you have a shed, or other garden buildings?",
+          value: null
         }
     ]
 };
@@ -34,21 +40,29 @@ new Vue({
         questionIndex: 0,
         // An array initialized with "false" values for each question
         // It means: "did the user answered correctly to the question n?" "no".
-        userResponses: Array(survey.questions.length).fill(false)
+        userResponses: [],
     },
     // The view will trigger these methods on click
     methods: {
         // Go to next question
         next: function() {
             this.questionIndex++;
+            this.store()
         },
         // Go to previous question
         prev: function() {
             this.questionIndex--;
         },
-        // Return "true" count in userResponses
-        score: function() {
-            return this.userResponses.filter(function(val) { return val }).length;
-        }
+        // Store the user's answers in a session.
+        store: function() {
+// // Store
+        sessionStorage.setItem("gardenLength", this.userResponses);
+        console.log(this.userResponses)
+        console.log(sessionStorage.getItem("gardenLength"))
+// // Retrieve
+document.getElementById("result").innerHTML = sessionStorage.getItem("gardenLength");
+        },
+        // Open up a new accordion dropdown with the results in it
+        showResults: function () {}
     }
 });
