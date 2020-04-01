6dee605ca18561b82b47f6c6271e05f4a6e1d489
Samuel Hopkinson | Netmatters Ltd <samuel.hopkinson@netmatters.local>
2020-04-01 12:32:56 +0100

    Continuing building functionality.
    Adding vue control of the canvas.

--- readyreck.js
+++ readyreck.js
@@ -1,9 +1,10 @@
 // Canvas instructions
 
-let canvas = document.querySelector("canvas");
-let context = canvas.getContext("2d");
-context.fillStyle = "red";
-context.fillRect(10, 10, 100, 50);
+// var c = document.getElementById("canvas");
+// var ctx = c.getContext("2d");
+// ctx.beginPath();
+// ctx.rect(20, 20, 150, 100);
+// ctx.stroke();
 
 // Create a quiz object with a title and two questions.
 // A question has one or more answer, and one or more is valid.
@@ -27,7 +28,7 @@ var survey = {
             }
         },
         {
-          text: "Do you have a shed, or other garden buildings?",
+            text: "Do you have a shed, or other garden buildings?",
             answers: {
                 possess: {
                     key: 2,
@@ -35,12 +36,12 @@ var survey = {
                     value: null
                 },
                 shedLength: {
-                  key: 3,
+                    key: 3,
                     label: "Length (in metres)",
                     value: null
                 },
                 shedWidth: {
-                  key: 4,
+                    key: 4,
                     label: "Width (in metres)",
                     value: null
                 }
@@ -55,55 +56,70 @@ new Vue({
         survey: survey,
         questionIndex: 0,
         userResponses: [],
-        stageOne: null,
-        stageTwo: null,
+        stageOne: false,
+        stageTwo: false,
         isDanger: false
     },
-
+    mounted: function () {
+        var c = document.getElementById("c");
+        var ctx = c.getContext("2d");
+        this.vueCanvas = ctx;
+    },
     methods: {
         // Go to next question, assuming it's been answered.
-        next: function() {
+        next: function () {
             let r = this.userResponses;
+            if (!this.stageOne) {
                 if (r[0] && r[1]) {
                     this.questionIndex++;
                     this.stageOne = true;
                     this.isDanger = false;
                     this.store();
-                } else if (!this.stageOne && !r[0] && !r[1]) {
+                } else {
                     this.isDanger = true;
                 }
+            }
 
-                if (this.stageOne) {
-                    r[2] = "Not Set";
+            // Question index 1.
+
+            if (this.stageOne && !this.stageTwo) {
+                this.isDanger = false;
+                if (r[2]) {
+                    if (r[3] && r[4]) {
+                        this.questionIndex++;
+                        this.stageTwo = true;
+                        this.isDanger = false;
+                        this.store();
+                    } else {
+                        this.isDanger = true;
+                        // TODO: ADD MESSAGE "If sheds are selected, please enter number".
+                    }
                 }
-
-                if (this.stageOne && r[2] && r[3] && r[4]) {
-                    console.log(r[3])
-                    console.log(r[4])
-                    this.questionIndex++;
-                    this.stageTwo = true;
-                    this.isDanger = false;
-                    this.store();
-                } else if (!this.stageTwo && (r[2] != "Not Set")) {
-                    this.isDanger = true;
-                    r[2] = null;
-                }
-
+            }
         },
         // Go to previous question
-        prev: function() {
+        prev: function () {
             this.questionIndex--;
+            if (this.stageOne) {
+                this.stageOne = false;
+            }
+
+            if (this.stageTwo) {
+                this.stageTwo = false;
+            }
+        },
+        skip: function () {
+            this.questionIndex++;
         },
         // Store the user's answers in a session.
-        store: function() {
+        store: function () {
 // // Store
-        sessionStorage.setItem("gardenLength", this.userResponses);
-        console.log(this.userResponses)
-        console.log(sessionStorage.getItem("gardenLength"))
+            sessionStorage.setItem("gardenLength", this.userResponses);
 // // Retrieve
-document.getElementById("result").innerHTML = sessionStorage.getItem("gardenLength");
+            document.getElementById("result").innerHTML = sessionStorage.getItem("gardenLength");
         },
         // Open up a new accordion dropdown with the results in it
-        showResults: function () {}
+        showResults: function () {
+        }
     }
 });
